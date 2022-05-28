022b0c9360c03b385b5b375d31c462f15b6a70e9
Wotox2 <mahdikamaly50@gmail.com>
2022-05-28 12:00:19 +0430

    Search 50%

--- src/js/search/Search.jsx
+++ src/js/search/Search.jsx
@@ -1,5 +1,5 @@
 
-
+import { Icon } from '@iconify/react';
 
 const Search = () => {
     return (
@@ -12,7 +12,142 @@ const Search = () => {
             </div>
             <div className="search-body page-grid-style-responsive">
                 <div className="left">
-                    Left
+                    <div className="left-header">
+                        <h1>Search Something</h1>
+                    </div>
+                    <div className="left-body">
+                        <div className="search-bar">
+                            <Icon icon="ep:search" />
+                            <input type="text" />
+                        </div>
+                        <div className="top-searches">
+                            <div className="top-searches-header">
+                                <h1>Top Searches</h1>
+                            </div>
+                            <div className="top-searches-body">
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/1.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            99 Chestnut Hill Ave
+                                        </div>
+                                        <div className="item-address">
+                                            Brighton, MA 02135
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/2.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/3.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/4.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/5.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/6.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/7.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-header">
+                                        <div className="item-image">
+                                            <img src={require('../images/search/1.jpg')} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="item-body">
+                                        <div className="item-name">
+                                            3738 Briarcliff Rd
+                                        </div>
+                                        <div className="item-address">
+                                            Atlanta, GA 30345
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="right">
                     right
